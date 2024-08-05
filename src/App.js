b305cb23f0e8b8369d2bcaafb31bb079dd5c3bdf
Nazib Akbar <najibfahrunaakbar123@gmail.com>
2024-08-05 14:16:59 +0700

    update : add insight components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import Home from "./pages/Home";
 
 function App() {
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" Component={Home} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Router>
+      <SpeedInsights />
+    </>
   );
 }
 
